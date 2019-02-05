fc47c759e60157ff782046e27512279c2ff5e47a
Bestony <best.tony@foxmail.com>
2019-02-05 12:46:22 +0800

    feat: export directory

--- bin/cli.js
+++ bin/cli.js
@@ -5,10 +5,11 @@
 const program = require('commander');
 const fs = require('fs');
 const path = require('path');
-const parser = require(path.join(__dirname, "../lib/parser.js"))
+const parser = require('../lib/parser.js')
 
 program
     .version('1.0.0','-v, --version');
+
 
 program
     .command('parse <file> [outputfile]')
@@ -25,5 +26,45 @@ program
         console.log(result)
       }
     });
+
+program
+    .command('dir <dir>')
+    .description("Parse all md File in specific Directory, and set it into a File")
+    .option('--platform [platform]','Specific Platform, Like common、osx、windows、linux、sunos')
+    .action(function(dir,options){
+      /**
+       * 路径处理
+       * @type {[type]}
+       */
+      let dirPath = dir;
+      if (!path.isAbsolute(dir)){
+        let dirPath = path.join(__dirname, dir)
+      }
+
+      let result = [];
+
+      /**
+       * 目录文件处理
+       * @type {[type]}
+       */
+      let directoryArray = fs.readdirSync(dirPath)
+      let outputPath = dirPath + '/tldr.json'
+
+      directoryArray.map(item => {
+        var info = fs.statSync(dirPath+"/"+item)
+        if(info.isFile() && path.extname(item) == '.md'){
+            let content = fs.readFileSync(dirPath+"/"+item,'utf8');
+            let tldrObj = parser.parse(content);
+
+            if (options.platform){
+              tldrObj.platform = options.platform
+            }
+            fs.appendFileSync(outputPath,JSON.stringify(tldrObj))
+        }
+      })
+
+      console.log("Export at " + outputPath);
+
+    })
 
 program.parse(process.argv);
